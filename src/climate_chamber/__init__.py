72d337a7d2719f0efb4994ce84b099ac444079ff
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2024-05-22 15:42:22 +0200

    climate_chamber: expose all data classes

--- src/climate_chamber/__init__.py
+++ src/climate_chamber/__init__.py
@@ -1,5 +1,19 @@
 from .climate_chamber import ClimateChamber
-from .data_classes import HumidityStatus, TemperatureStatus
+from .data_classes import (
+    HeatersStatus,
+    HumidityStatus,
+    OperationMode,
+    TemperatureStatus,
+    TestAreaState,
+)
 from .exceptions import SettingError
 
-__all__ = ["ClimateChamber", "HumidityStatus", "TemperatureStatus", "SettingError"]
+__all__ = [
+    "ClimateChamber",
+    "HumidityStatus",
+    "TemperatureStatus",
+    "SettingError",
+    "HeatersStatus",
+    "OperationMode",
+    "TestAreaState",
+]
